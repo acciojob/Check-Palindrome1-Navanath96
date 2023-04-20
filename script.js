de9295ed4811a58a7c04ed59268f72a5da470aa4
Navanath96 <117847388+Navanath96@users.noreply.github.com>
2023-04-20 22:10:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,8 @@
 function palindrome(str){
 for(let i=0; i<str.length; i++){
 	for(let j=str.length-1; j>=0; j--){
-		if(arr[i]==arr[j]){
-			j++;
-			i++;
+		if(arr[i]===arr[j]){
+			break;
 		}
 		else{
 			return false;
