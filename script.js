bb7754946ed43efd3dd0b8fa50afd6489e3ab1ed
Navanath96 <117847388+Navanath96@users.noreply.github.com>
2023-04-20 22:08:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+for(let i=0; i<str.length; i++){
+	for(let j=str.length-1; j>=0; j--){
+		if(arr[i]==arr[j]){
+			j++;
+			i++;
+		}
+		else{
+			return false;
+		}
+	}
+}
+	return true;
 }
 module.exports = palindrome
